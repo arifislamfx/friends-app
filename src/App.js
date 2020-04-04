3f9bb40655e7d8bbcee29f9494ab7a7db32a05b0
arifislamfx <arifislammfx@gmail.com>
2020-04-04 16:18:32 +0800

    done

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './component/Header/Header';
 import User from './component/User/User';
+import { Container } from 'react-bootstrap';
 
 
 const App = () => {
   return (
     <div className="App">
+      <Container fluid>
       <div className="bg bg-info">
       <Header></Header>
       <br/>
       <User></User>
       </div>
-    
+      </Container>
     </div>
   );
 };
